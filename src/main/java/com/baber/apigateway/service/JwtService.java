6babd5a424b388f5363ea248fd34400e3ab13b50
janindu3 <janindu@vega.lk>
2023-06-16 12:40:35 +0530

    added login and register

---
+++ src/main/java/com/baber/apigateway/service/JwtService.java
@@ -0,0 +1,32 @@
+package com.baber.apigateway.service;
+ 
+ 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+
+    public void validateToken(final String token) {
+     Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
